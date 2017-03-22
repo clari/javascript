be3d7699edc3739ae5fcb8e2b7a2c93280ee49f6
Matthew King <mking-clari@users.noreply.github.com>
2017-03-21 18:07:56 -0700

    add eslint rule

--- packages/eslint-config-clari/index.js
+++ packages/eslint-config-clari/index.js
@@ -6,9 +6,17 @@ module.exports = {
     jest: true,
   },
   rules: {
+    // don't require this for event handlers
+    'class-methods-use-this': 'off',
     'global-require': 'off',
+    // don't warn about missing extensions on externals
+    'import/extensions': 'off',
+    // don't require newline if it is a webpack require
+    'import/newline-after-import': 'off',
     'import/no-dynamic-require': 'off',
     'import/no-extraneous-dependencies': 'off',
+    // don't warn about unresolved packages for externals
+    'import/no-unresolved': 'off',
     // allow export and export default for connected component tests
     'import/no-named-as-default': 'off',
     'import/prefer-default-export': 'off',
@@ -23,6 +31,10 @@ module.exports = {
     'no-return-assign': 'off',
     'no-trailing-spaces': 'off',
     'no-underscore-dangle': 'off',
+    // allow style object
+    'react/forbid-prop-types': 'off',
+    // not all objects have a natural key
+    'react/no-array-index-key': 'off',
     'react/no-unused-prop-types': 'off',
     'react/require-default-props': 'off',
     'react/sort-comp': 'off',
